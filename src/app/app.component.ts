679bc78237de6fe30c09ccaebbc9fba70f9faf24
Lara Newsom <laramnewsom@gmail.com>
2025-05-05 16:13:18 -0500

    ran schematic to bootstrap as standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,20 @@ import { Component } from '@angular/core';
 import { LOGGER_TOKEN } from './tokens/logger-token';
 import { LoggerService } from './services/logger.service';
 import { BUSINESS_NAME } from './constants';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  providers: [
-    {
-      provide: LOGGER_TOKEN,
-      useClass: LoggerService,
-    }
-  ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    providers: [
+        {
+            provide: LOGGER_TOKEN,
+            useClass: LoggerService,
+        }
+    ],
+    standalone: true,
+    imports: [HeaderComponent, RouterOutlet]
 })
 export class AppComponent {
   readonly BUSINESS_NAME = BUSINESS_NAME;

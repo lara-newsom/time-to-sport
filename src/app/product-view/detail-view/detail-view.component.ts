13d32b557557bf5d0ffd21ead2fd5b5961506728
Lara Newsom <laramnewsom@gmail.com>
2025-05-11 18:11:37 -0500

    updated Detail View to use optimzed image

--- src/app/product-view/detail-view/detail-view.component.ts
+++ src/app/product-view/detail-view/detail-view.component.ts
@@ -1,7 +1,18 @@
-import { Component, Input, inject } from '@angular/core';
+import { AsyncPipe, CurrencyPipe, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { AddToCartButtonModule } from 'src/app/shared-ui/add-to-cart-button/add-to-cart-button.module';
 
 @Component({
+  standalone: true,
+  imports: [
+    NgIf,
+    NgOptimizedImage,
+    NgFor,
+    AsyncPipe,
+    CurrencyPipe,
+    AddToCartButtonModule
+  ],
   selector: 'app-detail-view',
   templateUrl: './detail-view.component.html',
   styleUrls: ['./detail-view.component.scss'],

498abdebf93cb139b961c65e2d7ca2172c3f6199
Lara Newsom <laramnewsom@gmail.com>
2025-05-16 12:28:08 -0500

    updated components to OnPush

--- src/app/shared-ui/sub-section/sub-section.component.ts
+++ src/app/shared-ui/sub-section/sub-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { NgFor } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -14,7 +14,8 @@ import { toSignal } from '@angular/core/rxjs-interop';
   ],
   selector: 'app-sub-section',
   templateUrl: './sub-section.component.html',
-  styleUrls: ['./sub-section.component.scss']
+  styleUrls: ['./sub-section.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubSectionComponent {
   protected readonly productService = inject(ProductService);

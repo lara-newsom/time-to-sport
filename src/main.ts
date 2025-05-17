d6bda0ee9ed1d312e77093aee2e52213a6186871
Lara Newsom <laramnewsom@gmail.com>
2025-05-17 12:33:04 -0500

    updated to event coalescing

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { importProvidersFrom } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { CommonModule } from '@angular/common';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { LoggerService } from './app/services/logger.service';
 import { LOGGER_TOKEN } from './app/tokens/logger-token';
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, CommonModule),
         {
             provide: LOGGER_TOKEN,
             useClass: LoggerService
         },
         provideRouter(routes, withComponentInputBinding()),
-        provideAnimations()
+        provideAnimations(),
+        provideZoneChangeDetection({ eventCoalescing: true })
     ]
 })
   .catch(err => console.error(err));

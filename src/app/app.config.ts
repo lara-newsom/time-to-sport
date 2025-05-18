5e554a87ba3e08f3932cf46c49f61a6015b2607f
Lara Newsom <laramnewsom@gmail.com>
2025-05-18 11:16:31 -0500

    added ssr to applicaiton

---
+++ src/app/app.config.ts
@@ -0,0 +1,23 @@
+import { provideZoneChangeDetection } from '@angular/core';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
+import { LoggerService } from './services/logger.service';
+import { LOGGER_TOKEN } from './tokens/logger-token';
+
+export const appConfig = {
+  providers: [
+    {
+      provide: LOGGER_TOKEN,
+      useClass: LoggerService,
+    },
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideClientHydration(withEventReplay()),
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { provideZoneChangeDetection } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app/app.routes';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { LoggerService } from './app/services/logger.service';
-import { LOGGER_TOKEN } from './app/tokens/logger-token';
+import {
+  bootstrapApplication,
+} from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        {
-            provide: LOGGER_TOKEN,
-            useClass: LoggerService
-        },
-        provideRouter(routes, withComponentInputBinding()),
-        provideAnimations(),
-        provideZoneChangeDetection({ eventCoalescing: true })
-    ]
-})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);

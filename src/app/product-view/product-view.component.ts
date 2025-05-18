9632e7bc9f7671f4451a3e1fbbccf87dc9a84f3a
Lara Newsom <laramnewsom@gmail.com>
2025-05-18 15:59:00 -0500

    implemented afterNextRender hook

--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -5,27 +5,28 @@ import {
   OnDestroy,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
-} from '@angular/core';
-import { ProductService } from '../services/product.service';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { filter, takeUntil } from 'rxjs/operators';
-import { LOGGER_TOKEN } from '../tokens/logger-token';
-import { ReplaySubject } from 'rxjs';
-import { SideMenuComponent } from './side-menu/side-menu.component';
-import { TableViewComponent } from './table-view/table-view.component';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
+  afterNextRender,
+} from "@angular/core";
+import { ProductService } from "../services/product.service";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
+import { filter, takeUntil } from "rxjs/operators";
+import { LOGGER_TOKEN } from "../tokens/logger-token";
+import { ReplaySubject } from "rxjs";
+import { SideMenuComponent } from "./side-menu/side-menu.component";
+import { TableViewComponent } from "./table-view/table-view.component";
+import { MatSlideToggle } from "@angular/material/slide-toggle";
 
 @Component({
-    selector: 'app-product-view',
-    templateUrl: './product-view.component.html',
-    styleUrls: ['./product-view.component.scss'],
-    imports: [
-        MatSlideToggle,
-        TableViewComponent,
-        SideMenuComponent,
-        RouterOutlet,
-    ],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-product-view",
+  templateUrl: "./product-view.component.html",
+  styleUrls: ["./product-view.component.scss"],
+  imports: [
+    MatSlideToggle,
+    TableViewComponent,
+    SideMenuComponent,
+    RouterOutlet,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductViewComponent implements OnDestroy {
   @Input() set categoryId(val: string) {
@@ -39,18 +40,20 @@ export class ProductViewComponent implements OnDestroy {
   private readonly destroyed$ = new ReplaySubject<void>(1);
 
   constructor() {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        takeUntil(this.destroyed$)
-      )
-      .subscribe(() => {
-        const content = document.querySelector<HTMLElement>('#productDetail');
-        if (content) {
-          content.focus();
-        }
-        this.cd.markForCheck();
-      });
+    afterNextRender(() => {
+      this.router.events
+        .pipe(
+          filter((event) => event instanceof NavigationEnd),
+          takeUntil(this.destroyed$)
+        )
+        .subscribe(() => {
+          const content = document.querySelector<HTMLElement>("#productDetail");
+          if (content) {
+            content.focus();
+          }
+          this.cd.markForCheck();
+        });
+    });
   }
 
   switchView() {
